9860059971c8365dc3df209f9da1103b5ff9e480
Kevin VanEvery <kvanever@gmail.com>
2016-09-20 04:43:38 -0700

    add an array of one through twenty five as strings to use as a fake data source

---
+++ Play Anywhere Bingo/BingoListModel.swift
@@ -0,0 +1,10 @@
+//
+//  BingoListModel.swift
+//  Play Anywhere Bingo
+//
+//  Created by Kevin VanEvery on 9/20/16.
+//  Copyright © 2016 Kevin VanEvery. All rights reserved.
+//
+
+import Foundation
+
