d99fe796287af2095074b6d8d00d47d21e52aee5
Kevin VanEvery <kvanever@gmail.com>
2016-09-20 04:33:02 -0700

    Convert to Swift 3

--- Play Anywhere BingoTests/Play_Anywhere_BingoTests.swift
+++ Play Anywhere BingoTests/Play_Anywhere_BingoTests.swift
@@ -28,7 +28,7 @@ class Play_Anywhere_BingoTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
